831b86f4734a2e1f4cb3389d5615d420fb8c8d81
mtheuslipe <matheusfelipe.mf454@gmail.com>
2022-08-12 13:55:20 -0300

    primeiro projeto em java

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+
+
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	Gato gato= new Gato("", "", 0);
+
+	public static void main(String[] args) {
+		System.out.println("Hello World." );
+	}
+}
